8a360a05f6d0c19690178552532784bf1999045f
phani <phanidharguttikonda0@gmail.com>
2025-03-01 20:55:26 +0530

    added controllers for creating stories and getting views

---
+++ backend/src/Services/Stories.ts
@@ -0,0 +1,44 @@
+import { PrismaClient } from "@prisma/client";
+import { getaccountType, isFollowing } from "./Posts";
+
+const prisma =  new PrismaClient() ;
+
+export async function getUserId(storyId: number){
+    try{
+        
+        const result = await prisma.stories.findUnique({
+            where:{
+                story_id: storyId
+            },
+            select:{
+                user_id: true,          
+            }
+        }) ;
+        return result?.user_id ;
+    }catch(err){
+        console.log(`Error was ${err}`) ;
+        return -1;
+    }
+
+}
+
+export async function isUserValid(userId: number, storyId: number){
+
+    try{
+        const owner_id = await getUserId(storyId) as number;
+        if(owner_id === -1) return false ;
+        const accountType = await getaccountType(owner_id) ;
+        if(accountType === "PRIVATE"){
+            // know we need to check whether the user is following or not
+            const result = await isFollowing(userId, owner_id) ;
+            if(result) return true;
+            else return false ;
+        }
+        else return true;
+    }catch(err){
+        console.log(`Error was ${err}`) ;
+        return false;
+    }
+
+
+}
--- backend/src/controllers/Stories.ts
+++ backend/src/controllers/Stories.ts
@@ -1,24 +1,110 @@
 import { NextFunction, Response } from "express";
 import { AuthenticatedRequest } from "../types/Profile";
+import getUserId from "../Services/getUserId";
+import { PrismaClient } from "@prisma/client";
 
-export async function createStory(req: AuthenticatedRequest, res: Response){}
+const prisma = new PrismaClient() ;
+
+export async function createStory(req: AuthenticatedRequest, res: Response){
+
+    try{
+        const user_id = await getUserId(req.user?.username || "") as number;
+        const url = req.body.url ;
+        const type = req.body.type ;
+        const result = await prisma.stories.create({
+            data:{
+                CDNURL: url,
+                type: type,
+                user_id: user_id,
+            }
+        }) ;
+        res.json({message: "successfully created story", data:true}) ;
+    }catch(err){
+        console.log(`Error Occured ${err}`) ;
+        res.json({message: "error occured", data: null}) ;
+    }
+
+}
 
 
-export async function getViews(req: AuthenticatedRequest, res: Response) {}
+export async function getViews(req: AuthenticatedRequest, res: Response) {
+    try{
+        const story_id = req.body.story_id ;
+        const result1 = await prisma.stories.findFirst({
+            where:{
+                story_id: story_id
+            },
+            select:{
+                user_id: true,
+            }
+        }) ;
+        const user_id = await getUserId(req.user?.username || "") as number ;
+        if(user_id === result1?.user_id){
+            const result = await prisma.storyViews.findMany({
+                where:{
+                    story_id: story_id
+                },
+                select:{
+                    user:{
+                        select:{
+                           user_id: true,
+                           username: true,
+                           profilePicture: true 
+                        }
+                    }
+                }
+            }) ;
+            console.log(`result was ${result}`) ;
+            res.json({message: "success", data: result}) ;
+        }else{
+            res.json({message: "unauthorized access not an owner", data: null}) ;
+        }
+    }catch(err){
+        console.log(`Error Occured ${err}`) ;
+        res.json({message: "error occured", data: null}) ;
+    }
+}
 
 
-export async function getViewsCount(req: AuthenticatedRequest, res: Response){}
+export async function getViewsCount(req: AuthenticatedRequest, res: Response){
+    try{
+        // there is no need to views count as getViews return an array of users
+        // that length will be views count
+    }catch(err){
+        console.log(`Error Occured ${err}`) ;
+        res.json({message: "error occured", data: null}) ;
+    }
+}
 
 
-export async function deleteStory(req: AuthenticatedRequest, res: Response){}
+export async function deleteStory(req: AuthenticatedRequest, res: Response){
+    try{
+
+    }catch(err){
+        console.log(`Error Occured ${err}`) ;
+        res.json({message: "error occured", data: null}) ;
+    }
+}
 
 
 export async function getStories(req: AuthenticatedRequest, res:Response){
     // returns all the stories of the followers along with their profile pic and username
     // returns all the stories of the user
+    try{
+
+    }catch(err){
+        console.log(`Error Occured ${err}`) ;
+        res.json({message: "error occured", data: null}) ;
+    }
 }
 
 
 export async function postView(req: AuthenticatedRequest, res: Response){
     // posts a view for the story
+    try{
+
+    }catch(err){
+        console.log(`Error Occured ${err}`) ;
+        res.json({message: "error occured", data: null}) ;
+    }
 }
